c2d2266bb1c0f9574fc1fdff923702559421b577
Nuttapat Kirawittaya <nuttapat.kirawittaya@gmail.com>
2020-06-10 09:37:53 +0800

    :sparkles: Index page list deck

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link, graphql, PageProps } from 'gatsby'
+import moment from 'moment'
 
 import Page from '../components/Page'
 import Container from '../components/Container'
@@ -26,30 +27,97 @@ type Data = {
       }
     }[]
   }
+  allDeck: {
+    edges: {
+      node: {
+        slug: string
+        frontmatter: {
+          title: string
+          date: string
+          description: string
+        }
+      }
+    }[]
+  }
 }
+
+type Item =
+  | {
+      type: 'Post'
+      date: moment.Moment
+      node: Data['allMarkdownRemark']['edges'][0]['node']
+    }
+  | {
+      type: 'Deck'
+      date: moment.Moment
+      node: Data['allDeck']['edges'][0]['node']
+    }
 
 const IndexPage: React.FC<PageProps<Data>> = ({ data }) => {
   const posts = data.allMarkdownRemark.edges
+  const decks = data.allDeck.edges
+  const allPosts: Item[] = posts
+    .map<Item>(({ node }) => ({
+      type: 'Post',
+      date: moment(node.frontmatter.date),
+      node
+    }))
+    .concat(
+      decks.map(({ node }) => ({
+        type: 'Deck',
+        date: moment(node.frontmatter.date),
+        node
+      }))
+    )
+    .sort((a: Item, b: Item) => b.date.valueOf() - a.date.valueOf())
+
   return (
     <IndexLayout>
       <Page>
         <Container>
-          {posts.map(({ node }) => {
-            const title = node.frontmatter.title || node.fields.slug
+          {allPosts.map(item => {
+            if (item.type === 'Post') {
+              const title = item.node.frontmatter.title || item.node.fields.slug
+              return (
+                <article key={item.node.fields.slug}>
+                  <header>
+                    <h3>
+                      <Link
+                        style={{ boxShadow: `none` }}
+                        to={item.node.fields.slug}
+                      >
+                        {title}
+                      </Link>
+                    </h3>
+                    <small>{item.node.frontmatter.date}</small>
+                  </header>
+                  <section>
+                    <p
+                      dangerouslySetInnerHTML={{
+                        __html:
+                          item.node.frontmatter.description || item.node.excerpt
+                      }}
+                    />
+                  </section>
+                </article>
+              )
+            }
+
+            const { title, description } = item.node.frontmatter
             return (
-              <article key={node.fields.slug}>
+              <article key={item.node.slug}>
                 <header>
                   <h3>
-                    <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                    <Link style={{ boxShadow: `none` }} to={item.node.slug}>
                       {title}
                     </Link>
                   </h3>
-                  <small>{node.frontmatter.date}</small>
+                  <small>{item.node.frontmatter.date}</small>
                 </header>
                 <section>
                   <p
                     dangerouslySetInnerHTML={{
-                      __html: node.frontmatter.description || node.excerpt
+                      __html: description
                     }}
                   />
                 </section>
@@ -71,6 +139,18 @@ export const pageQuery = graphql`
           fields {
             slug
           }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+    }
+    allDeck(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          slug
           frontmatter {
             date(formatString: "MMMM DD, YYYY")
             title
