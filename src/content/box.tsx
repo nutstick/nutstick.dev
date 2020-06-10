1241220363620dbf33b7cfc1db234cf87d89c3a7
Nuttapat Kirawittaya <nuttapat.kirawittaya@gmail.com>
2020-06-10 09:38:45 +0800

    :pencil: [sneak-peak-renanimated-2] Animation + more sameple code

---
+++ src/content/box.tsx
@@ -0,0 +1,44 @@
+/** @jsx jsx */
+import React from 'react'
+import { jsx, css, keyframes } from '@emotion/core'
+
+const smooth = keyframes`
+  0% {
+    transform: translateX(-200px);
+  }
+  100% {
+    transform: translateX(200px);
+  }
+`
+
+const lag = keyframes`
+  0% {
+    transform: translateX(-200px);
+  }
+  76% {
+    transform: translateX(104px);
+  }
+  77% {
+    transform: translateX(104px);
+  }
+  100% {
+    transform: translateX(200px);
+  }
+`
+
+const Box: React.FC<{ animation: 'smooth' | 'lag' }> = ({ animation }) => {
+  return (
+    <div
+      css={css`
+        width: 100px;
+        height: 100px;
+        background-color: #379683;
+        animation: ${animation === 'smooth' ? smooth : lag} 2.5s
+          ${animation === 'smooth' ? '' : 'steps(15)'} infinite;
+        margin: 3em auto;
+      `}
+    />
+  )
+}
+
+export default Box
