a56a1a96a702e77142537a9d7b451d436517e941
Nuttapat Kirawittaya <nuttapat.kirawittaya@gmail.com>
2022-01-04 21:23:12 +0800

    :card_file_box: dynamic retrieve mdx using fs

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,39 +2,12 @@ import React from 'react';
 import BlogList from '../components/blog-list';
 import Profile from '../components/profile/index.client';
 import Page from '../layouts/page';
+import fs from 'fs';
 
 import type { NextPage } from 'next';
 import type { BlogPost } from '../interfaces';
 
-const allPosts: BlogPost[] = [
-  {
-    slug: 'react-tutorial-1-bootstrap',
-    frontmatter: require('./react-tutorial-1-bootstrap.mdx').frontmatter,
-  },
-  {
-    slug: 'react-tutorial-2-first-react-component',
-    frontmatter: require('./react-tutorial-2-first-react-component.mdx')
-      .frontmatter,
-  },
-  {
-    slug: 'react-tutorial-3-meet-use-state',
-    frontmatter: require('./react-tutorial-3-meet-use-state.mdx').frontmatter,
-  },
-  {
-    slug: 'react-tutorial-4-fetch',
-    frontmatter: require('./react-tutorial-4-fetch.mdx').frontmatter,
-  },
-  {
-    slug: 'react-native-metro',
-    frontmatter: require('./react-native-metro.mdx').frontmatter,
-  },
-  {
-    slug: 'sneak-peak-reanimated-2',
-    frontmatter: require('./sneak-peak-reanimated-2.mdx').frontmatter,
-  },
-];
-
-const Home: NextPage = () => {
+const Home: NextPage<{ allPosts: BlogPost[] }> = ({ allPosts }) => {
   return (
     <Page>
       <Profile />
@@ -42,5 +15,29 @@ const Home: NextPage = () => {
     </Page>
   );
 };
+
+export async function getStaticProps() {
+  const allPosts = await Promise.all(
+    fs
+      .readdirSync('./pages')
+      .filter((file) => /\.mdx$/.test(file))
+      .map(async (file): Promise<BlogPost> => {
+        const { frontmatter } = await import(`./${file}`);
+        return {
+          slug: file.replace('.mdx', ''),
+          frontmatter,
+        };
+      })
+  );
+  return {
+    props: {
+      allPosts: allPosts.sort(
+        (a, b) =>
+          new Date(b.frontmatter.date).getTime() -
+          new Date(a.frontmatter.date).getTime()
+      ),
+    },
+  };
+}
 
 export default Home;
