98922adb0df9c8466204f05c74cdbb8a5cc3e33d
Nuttapat Kirawittaya <nuttapat.kirawittaya@gmail.com>
2022-01-30 12:54:05 +0800

    :chart_with_upwards_trend: integrate with google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,45 @@
+import { useEffect } from 'react';
+import Script from 'next/script';
+import { useRouter } from 'next/router';
 import Layout from '../layouts';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const onRouteChange = (url: URL) => {
+      if (window.gtag) {
+        window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS!, {
+          page_path: url,
+        });
+      }
+    };
+    router.events.on('routeChangeComplete', onRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChange);
+    };
+  }, [router.events]);
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+      <Script id="ga-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+        `}
+      </Script>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
