bc81316541c3fa60c337e270293ee15f703e9e95
Nuttapat Kirawittaya <nuttapat.kirawittaya@gmail.com>
2022-12-31 14:27:40 +0800

    :bug: fix: dialog not appearing after navigate

--- apps/wedding/components/layout/index.tsx
+++ apps/wedding/components/layout/index.tsx
@@ -1,6 +1,5 @@
 import { Dancing_Script, Lora, Noto_Sans_Thai } from '@next/font/google';
-import { PortalContext } from 'ariakit/portal';
-import { ReactNode, useMemo } from 'react';
+import { ReactNode } from 'react';
 
 const cormarantGaramond = Dancing_Script({
   weight: '400',
@@ -24,22 +23,12 @@ const notoSansThai = Noto_Sans_Thai({
 });
 
 function Layout({ children }: { children: ReactNode }) {
-  const container = useMemo(
-    () =>
-      typeof document !== 'undefined'
-        ? document.getElementById('container')
-        : null,
-    []
-  );
-
   return (
     <div
       id="container"
       className={`${lora.variable} ${notoSansThai.variable} ${cormarantGaramond.variable} font-sans`}
     >
-      <PortalContext.Provider value={container}>
-        {children}
-      </PortalContext.Provider>
+      {children}
     </div>
   );
 }
